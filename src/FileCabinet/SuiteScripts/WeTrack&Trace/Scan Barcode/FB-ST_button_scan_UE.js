db26ec3007722145637189bdfc6b5e6e0cc56095
Dylan Michel Mendoza Hernández <dylan.mendoza@freebug.mx>
2023-09-07 18:09:24 -0600

    Update FB-ST_button_scan_UE.js
    Se agregaron condiciones para indicar cuando se puede escanear un PO y cuando no.

--- src/FileCabinet/SuiteScripts/WeTrack&Trace/Scan Barcode/FB-ST_button_scan_UE.js
+++ src/FileCabinet/SuiteScripts/WeTrack&Trace/Scan Barcode/FB-ST_button_scan_UE.js
@@ -8,7 +8,7 @@
  * @copyright Tekiio México 2023
  * 
  * Client              -> Tekiio
- * Last modification   -> 06/09/2023
+ * Last modification   -> 07/09/2023
  * Modified by         -> Dylan Mendoza <dylan.mendoza@freebug.mx>
  * Script in NS        -> FB - ST_Button_Scan_PO_UE <ID del registro>
  */
@@ -38,14 +38,19 @@ define(['N/log'],
                     var objRecord = scriptContext.newRecord;
                     var typeTransaction = objRecord.type;
                     var form = scriptContext.form;
-                    log.audit('type transaction', typeTransaction);
-                    log.debug({ title:'idTransaction', details:objRecord.id });
-                    form.addButton({
-                        id: 'custpage_fb_st_btn_scan_po',
-                        label: 'Scan and Receive',
-                        functionName: 'sendToScan("' + typeTransaction + '",'+ objRecord.id +')'
-                    });
-                    form.clientScriptModulePath = './FB-ST_button_scan_CS.js';
+                    var status = objRecord.getValue({fieldId: 'orderstatus'});
+                    var createdFrom = objRecord.getValue({fieldId: 'createdfrom'});
+                    if ( !createdFrom && (status == 'B' || status == 'E')) { // No es dropship - Pendiente de recibir - Parcialmente recivido
+                        log.debug({ title:'statusref', details:status });
+                        log.audit('type transaction', typeTransaction);
+                        log.debug({ title:'idTransaction', details:objRecord.id });
+                        form.addButton({
+                            id: 'custpage_fb_st_btn_scan_po',
+                            label: 'Scan and Receive',
+                            functionName: 'sendToScan("' + typeTransaction + '",'+ objRecord.id +')'
+                        });
+                        form.clientScriptModulePath = './FB-ST_button_scan_CS.js';
+                    }
                 }
             } catch (e) {
                 log.error('Error on before load', e);
